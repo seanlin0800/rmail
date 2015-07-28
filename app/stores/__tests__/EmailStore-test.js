0d73c2b05d8f18209df3e5592e16e02d840f9ae3
seanlin0800 <stonehomelaa@gmail.com>
2015-07-28 17:26:20 +0800

    Add unit tests for EmailStore

---
+++ app/stores/__tests__/EmailStore-test.js
@@ -0,0 +1,94 @@
+jest.dontMock('../EmailStore');
+
+describe('EmailStore', function() {
+
+  var EmailStore;
+  var ServerActionCreators;
+
+  beforeEach(function() {
+    EmailStore = require('../EmailStore');
+    ServerActionCreators = require('../../actions/ServerActionCreators');
+  });
+
+  it('should initialize with no mail items', function() {
+    var all = EmailStore.getAll();
+    expect(all).toEqual({});
+  });
+
+  describe('Actions', function() {
+    var MailThreadActionCreators;
+
+    beforeEach(function() {
+      ServerActionCreators.receiveAll([
+        {
+          id: 1,
+          name: 'sent',
+          emails: [
+            {
+              id: 1,
+              from: 'alice@rmail.com',
+              to: 'seanlin0800@rmail.com',
+              subject: 'Meeting',
+              body: 'hi',
+              isRead: false,
+              isStarred: false,
+              date: '2014-12-29T04:44:44Z'
+            }
+          ]
+        }
+      ]);
+      MailThreadActionCreators = require('../../actions/MailThreadActionCreators');
+      jest.mock('../../utils/APIUtils');
+    });
+
+    it('should initialize dataset on receiveAll', function() {
+      var all = EmailStore.getAll();
+      var keys = Object.keys(all);
+
+      expect(keys.length).toBe(1);
+      expect(all.sent.name).toEqual('sent');
+      expect(Object.keys(all.sent.emails).length).toBe(1);
+    });
+
+    it('should mark mail thread read on markThread', function() {
+      var mail;
+
+      MailThreadActionCreators.markThread('sent', 1, true);
+      mail = EmailStore.get('sent', 1);
+
+      expect(mail.isRead).toBe(true);
+    });
+
+    it('should delete an mail on delete', function() {
+      MailThreadActionCreators.delete('sent', 1);
+      expect(EmailStore.getAll().sent.emails).toEqual({});
+    });
+
+    it('should start an mail on ClickStar', function() {
+      var mail;
+
+      MailThreadActionCreators.clickStar({
+        name: 'sent',
+        id: 1,
+        val: false
+      });
+      mail = EmailStore.get('sent', 1);
+
+      expect(mail.isStarred).toBe(true);
+    });
+
+    it('should add an mail on sendMailSuccess', function() {
+      ServerActionCreators.sendMailSuccess({
+        id: 2,
+        from: 'seanlin0800@rmail.com',
+        to: 'alice@rmail.com',
+        subject: 'test',
+        body: '123',
+        date: '2015-12-29T04:44:44Z'
+      });
+
+      expect(Object.keys(EmailStore.getAll().sent.emails).length).toBe(2);
+    });
+  });
+
+});
